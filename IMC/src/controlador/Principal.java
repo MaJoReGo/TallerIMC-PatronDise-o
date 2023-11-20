b9343f2522f1433e61996cfc4ab012a75e45c253
Maria Jose Restrepo <restrepogomezmariajose@gmail.com>
2023-11-20 01:50:43 -0500

    Taller con Patron de diseño

---
+++ IMC/src/controlador/Principal.java
@@ -0,0 +1,13 @@
+package controlador;
+
+import gui.VentanaPrincipal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		VentanaPrincipal miVentana=
+				new VentanaPrincipal();
+		miVentana.setVisible(true);
+	}
+
+}
